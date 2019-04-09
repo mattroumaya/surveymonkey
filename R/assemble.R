55e8a389281ee346ccec35c4f6e916c31ec5f356
Sam Firke <samuel.firke@gmail.com>
2019-04-09 13:43:49 -0400

    now working with unique Q IDs instead of text
    swapping text back in at end.  Need to do in factorize.R now

--- R/assemble.R
+++ R/assemble.R
@@ -26,48 +26,60 @@ parse_survey <- function(surv_obj){
     dplyr::rename(open_response_text = answertext)
 
   final_x <- x %>%
-    dplyr::mutate(combined_text = dplyr::case_when(
-      #     !is.na(subquestion_text) ~ paste(stringr::str_trunc(heading, width = 40), subquestion_text, sep = " "),
+    dplyr::mutate(combined_text = dplyr::case_when( # most of what will eventually be column headers - but problematic for duplicated Qs
       !is.na(subquestion_text) ~ paste(heading, subquestion_text, sep = " "),
       !is.na(open_response_text) & question_type != "open_ended" ~ paste(heading, text, sep = " - "), # for "Other (please specify)"
       question_type %in% "multiple_choice" ~ paste(heading, text, sep = " - "), # expand multiple choice Qs to be one-column-per
       TRUE ~ heading)) %>%
-    dplyr::select(collector_id, recipient_id, response_id, question_type, combined_text, text, open_response_text, question_id) %>%
+    dplyr::mutate(unique_q_id = dplyr::case_when( # spread and join on this as it works for duplicated Q text
+      !is.na(subquestion_text) ~ paste(question_id, subquestion_id, sep = "_"),
+      !is.na(open_response_text) & question_type != "open_ended" ~ paste(question_id, choice_id, sep = "_"), # for "Other (please specify)"
+      question_type %in% "multiple_choice" ~ paste(question_id, choice_id, sep = "_"), # expand multiple choice Qs to be one-column-per
+      TRUE ~ question_id
+    )) %>%
     dplyr::mutate(text = dplyr::case_when(
       !is.na(open_response_text) ~ open_response_text, # replace with "Other" text when present
       TRUE ~ text)
-    )
+    ) %>%
+    dplyr::select(collector_id, recipient_id, response_id, question_type, combined_text, text, open_response_text, unique_q_id)
 
 
-  # If making unique any duplicated combined_text values for different question_id values, do that here -
-  # and also in factorize.R to make the modifications match
-
+  qid_text_crosswalk <- final_x %>%
+    distinct(unique_q_id, .keep_all = TRUE) %>%
+    select(unique_q_id, combined_text) %>%
+    mutate(unique_text = de_duplicate_names(combined_text))
 
   # remove non-multiple-choice answers that weren't selected.  Empty MC choices remain to generate empty columns.
   final_x <- final_x %>%
-    dplyr::filter(!(question_type != "multiple_choice" & is.na(response_id))) %>%
+    dplyr::filter(!(question_type != c("multiple_choice") & is.na(response_id))) %>%
     dplyr::select(-question_type, -open_response_text) # remove for spread
 
   # spread wide
   # get column order to reset to after spread makes alphabetical
-  col_names <- c(names(final_x)[!(names(final_x) %in% c("combined_text","text"))], unique(final_x$combined_text))
+  col_names <- c(names(final_x)[!(names(final_x) %in% c("combined_text","text", "unique_q_id"))], qid_text_crosswalk$unique_text)
 
   out <- final_x %>%
-    tidyr::spread(combined_text, text) %>%
+    select(-combined_text) %>%
+    mutate(unique_q_id = factor(unique_q_id, levels = qid_text_crosswalk$unique_q_id)) %>% # to spread unrepresented levels
+    tidyr::spread(unique_q_id, text, drop = FALSE) %>%
     dplyr::filter(!is.na(response_id))
 
-  ## Remove HTML tags?
-  # remove HTML tags from question text - there are many possibilities,
-  # I'm trying to be conservative in case <> contains user text but maybe offer
-  # a "remove_html_tags" arg
-  # MOVING THIS HERE, AFTER SPREAD, TO FACTORIZE COLS FIRST - need to refactor to adjust for move
-  # dplyr::mutate(combined_text = gsub("<span.*\">", "", combined_text),
-  #               combined_text = gsub("</span>", "", combined_text),
-  #               combined_text = gsub("<em>", "", combined_text),
-  #               combined_text = gsub("</em>", "", combined_text),
-  #               combined_text = gsub("<strong>", "", combined_text),
-  #               combined_text = gsub("</strong>", "", combined_text)) %>%
   out <- factorize_columns(surv_obj, out)
 
+  # reset to text names instead of numbers
+  # and then re-order to correct columns
+  names(out)[4:length(names(out))] <- qid_text_crosswalk$unique_text[qid_text_crosswalk$unique_q_id == names(out)[4:length(names(out))]]
   out[, col_names]
+}
+
+# Helper function for de-duplicating identical Q names
+# Input: the vector of names
+# Adapted from janitor::make_clean_names()
+de_duplicate_names <- function(x){
+  dupe_count <- vapply(seq_along(x), function(i) {
+    sum(x[i] == x[1:i])
+  }, integer(1))
+  x[dupe_count > 1] <- paste(x[dupe_count >
+                                 1], dupe_count[dupe_count > 1], sep = "_")
+  x
 }
