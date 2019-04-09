e9b52c8ebcf6eb6094c733ebb8b33f57408d053e
Sam Firke <samuel.firke@gmail.com>
2019-04-09 14:03:20 -0400

    about to remove old de-duplicated code that's obsolete

--- R/assemble.R
+++ R/assemble.R
@@ -64,6 +64,62 @@ parse_survey <- function(surv_obj){
     tidyr::spread(unique_q_id, text, drop = FALSE) %>%
     dplyr::filter(!is.na(response_id))
 
+  # Takes spread-out results data.frame and turns multiple choice cols into factors.  GH issue #12
+  # Doing this within the main function so it can see crosswalk
+  factorize_columns <- function(surv_obj, results_df){
+    # redundant with calls in master assembly function parse_responses - but these don't hit the API
+    choices <- survey_choices(surv_obj)
+    questions <- get_questions(surv_obj)
+
+    # this is to match question column formatting of main data.frame of results
+    # necessary, but FYI code is duplicated with that in parse_responses
+    # could merge this function into parse_results to streamline?
+    master_qs <- dplyr::inner_join(choices, questions) %>%
+      dplyr::mutate(combined_text = dplyr::case_when(
+        !is.na(subquestion_text) ~ paste(heading, subquestion_text, sep = " "),
+        position == 0 & question_type != "open_ended" ~ paste(heading, text, sep = " - "), # for "Other (please specify)" - I think position = 0 is consistent?
+        question_type %in% "multiple_choice" ~ paste(heading, text, sep = " - "),
+        TRUE ~ heading)) %>%
+      dplyr::mutate(unique_q_id = dplyr::case_when( # spread and join on this as it works for duplicated Q text
+        !is.na(subquestion_text) ~ paste(question_id, subquestion_id, sep = "_"),
+        position == 0 & question_type != "open_ended" ~ paste(question_id, choice_id, sep = "_"), # for "Other (please specify)"
+        question_type %in% "multiple_choice" ~ paste(question_id, choice_id, sep = "_"), # expand multiple choice Qs to be one-column-per
+        TRUE ~ question_id
+      ))
+
+    # De-duplicate text names to match the deduplication pre-spreading in the master parse_surveys function
+    de_dupe_crosswalk <- tibble::tibble(combined_text = unique(master_qs$combined_text))
+    de_dupe_crosswalk$new <- de_duplicate_names(de_dupe_crosswalk$combined_text)
+
+    master_qs <- dplyr::inner_join(master_qs, de_dupe_crosswalk) %>%
+      dplyr::select(-combined_text) %>%
+      dplyr::rename(combined_text = new) # replace old combined_text with new deduplicated version
+
+    # set a vector as a factor, if it has answer choices associated with its question text
+    set_factor_levels <- function(vec, col_name){
+
+      # fetch possible answer choices given a question's text
+      get_factor_levels <- function(col_name){
+        master_qs %>%
+          dplyr::filter(combined_text == col_name) %>%
+          dplyr::arrange(position) %>% # appears to always come from API in order but don't want to assume
+          dplyr::pull(text)
+      }
+
+      name_set <- get_factor_levels(col_name)
+      if(length(name_set) == 0){
+        return(vec)
+      } else {
+        factor(vec, levels = name_set)
+      }
+    }
+
+    out <- purrr::map2_dfc(results_df, names(results_df), set_factor_levels)
+    out
+
+  }
+
+
   out <- factorize_columns(surv_obj, out)
 
   # reset to text names instead of numbers
