87ca80030ec49f2789482b73eb79348894edafa0
Sam Firke <sam.firke@tntp.org>
2019-10-07 15:12:16 -0400

    fix erroneous comma in call to API in fetching survey

--- R/fetch_survey.R
+++ R/fetch_survey.R
@@ -29,8 +29,7 @@ fetch_survey_obj <- function(id,
                          'Content-Type'='application/json')
   p <- list("v3", survey = "surveys", id = id, details = "details")
 
-  out <- httr::GET(u, config = h, path = p, httr::user_agent("http://github.com/tntp/surveymonkey"),
-)
+  out <- httr::GET(u, config = h, path = p, httr::user_agent("http://github.com/tntp/surveymonkey"))
   message(paste0("you have ", out$headers$`x-ratelimit-app-global-day-remaining`, " requests left today before you hit the limit"))
   httr::stop_for_status(out)
   parsed_content <- httr::content(out, as = 'parsed')
