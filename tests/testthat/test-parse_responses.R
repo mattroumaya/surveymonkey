c9c35980ff06cfa20d959edcf403e063da20cbf0
Sam Firke <sam.firke@tntp.org>
2020-03-18 10:27:25 -0400

    fix relative filepaths for test parse_respondent_list data

--- tests/testthat/test-parse_responses.R
+++ tests/testthat/test-parse_responses.R
@@ -2,13 +2,13 @@
 # This is the result of a survey called Sample Survey for R Package API Testing
 # Managed by Dustin Pashouwer and Sam Firke
 # It's what's produced by the line responses <- parsed_content$data in get_responses.R
-responses_raw <- readRDS("tests/testthat/test_responses_raw.rds")
+responses_raw <- readRDS("test_responses_raw.rds")
 
 # Loads the results of parse_respondent_list(responses) when it was running correctly
 # Yes, this is tautological, but provides protection from breaking it going forward
 # Would be good to eventually add more exhaustive coverage of question types
 
-parsed_correctly <- readRDS("tests/testthat/parsed_responses.rds")
+parsed_correctly <- readRDS("parsed_responses.rds")
 
 test_that("responses parsed correctly", {
   expect_equal(
