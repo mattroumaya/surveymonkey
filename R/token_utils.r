36ca4b82a7590cb2886acc4075e14d50eeb07ce7
Jamie <jamie@jumpingrivers.com>
2022-01-09 21:06:06 +0000

    move token utils to separate file
    - better matches test file names

---
+++ R/token_utils.r
@@ -0,0 +1,29 @@
+#' Set Survey Monkey Oauth Token
+#'
+#' Sets the option 'sm_oauth_token' for the survey monkey API.
+#' Token will be used for authorization headers for requests.
+#'
+#' @param oauth_token Your survey monkey OAuth 2.0 token.
+#' @export
+set_token = function(oauth_token) {
+  options("sm_oauth_token" = oauth_token)
+}
+
+#' Retrieve set Survey Monkey OAuth Token
+#'
+#' Retrieves the currently set suvery monkey oauth token
+#' @export
+get_token = function() {
+  getOption("sm_oauth_token")
+}
+
+
+get_bearer_token = function(oauth_token = NULL) {
+  if (is.null(oauth_token)) {
+    stop(
+      "Must specify 'oauth_token'.
+      See https://github.com/tntp/surveymonkey#authentication for more info."
+    )
+  }
+  paste("bearer", oauth_token)
+}
