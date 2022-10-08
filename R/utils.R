c51d441f9ccaf8e5051e1665f9d6fa74ed7a1bf0
Neal Fultz <nfultz@gmail.com>
2022-10-08 10:08:22 -0700

    Update github urls.

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,9 @@
 # Generate a suggestion to submit a bug report at a specific URL
 # This is a function so that it can be easily changed if the repo URL changes
 
+#' @importFrom utils packageDescription
 file_bug_report_msg <- function() {
-  "file a bug report at https://github.com/tntp/surveymonkey/issues"
+  paste("file a bug report at", packageDescription("surveymonkey")$BugReports)
 }
 
 format_date = function(date) {
